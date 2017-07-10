c54fc4f9b304dbd10d72d5c3785e63aa8b873c0f
sysuzhyupeng <zhyupengmail2@163.com>
2017-07-10 21:22:33 +0800

    增加webpack-dev-server自动打开

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@
     ]
   }
 */
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+//htmlWebpackPlugin自动化生成项目中html页面
+var htmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 var webpack = require('webpack');
 //全局变量检测生产环境
@@ -65,20 +66,25 @@ var baseConfig = {
 	},
 	//插件（Plugins）是用来拓展Webpack功能的,插件并不直接操作单个文件，它直接对整个构建过程其作用。
 	plugins: [
+		//在脚本中加入变量
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(ENV)
 		}),
+		//压缩
 		new webpack.optimize.UglifyJsPlugin({
 			sourceMap: false,
 			mangle: false
-		})
+		}),
+		// new htmlWebpackPlugin({
+		// 	filename: 'index-[hash].html',
+		// 	template: 'dom.html',
+		// 	//把output文件注入到head标签中
+		// 	inject: 'head'
+		// })
 	]
 };
 //使用UglifyJS压缩
 if (ENV === 'production') {
 	// baseConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: false,mangle: false}));
-}
-devServer: {
-	inline: true
 }
 module.exports = baseConfig
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,1 @@
-const webpack = require('webpack');
-const path = require('path');
-
-module.exports = {
-    entry: __dirname + "/app/canvas.js",
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundlecanvas.js'
-    }
-};
+var path = require('path');var webpack = require('webpack');//全局变量检测生产环境var ENV = process.env.NODE_ENV;var PATHS = {	app: path.join(__dirname, 'app/canvas'),	dist: path.join(__dirname, 'dist')};var baseConfig = {	entry: [		PATHS.app,		'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',	],	output: {		path: PATHS.dist,		filename: '[name].js',	},	devtool: '#source-map',	module: {		rules: [{			test: /\.css$/,			use: [{				loader: 'style-loader'			}, {				loader: 'css-loader'			}]		}, {			test: /\.less$/,			use: [{				loader: 'style-loader'			}, {				loader: 'css-loader'			}, {				loader: 'less-loader'			}]		}, {			//识别url，将png替换成base64			test: /\.svg$/,			use: [{				loader: 'url-loader',				query: {					limit: 10000				}			}]		}]	},	//插件（Plugins）是用来拓展Webpack功能的,插件并不直接操作单个文件，它直接对整个构建过程其作用。	plugins: [		new webpack.DefinePlugin({			'process.env.NODE_ENV': JSON.stringify(ENV)		}),		new webpack.optimize.UglifyJsPlugin({			sourceMap: false,			mangle: false		}),		new webpack.HotModuleReplacementPlugin(),		new webpack.NoEmitOnErrorsPlugin()	]};module.exports = baseConfig
